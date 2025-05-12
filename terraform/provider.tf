5b3b4fd475e38ca509348d2370e0d89f9c8a2c4a
Jegorik <jegorik@aroundacloud.co.uk>
2025-05-12 18:26:10 +0300

    Fix formatting in provider.tf for consistency

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -11,7 +11,7 @@ terraform {
     # Databricks provider for managing Databricks resources
     databricks = {
       source  = "databricks/databricks"
-      version = "1.77"  # Using the latest version (as of May 2025)
+      version = "1.77" # Using the latest version (as of May 2025)
     }
   }
 
@@ -28,9 +28,9 @@ terraform {
 provider "databricks" {
   host                 = var.databricks_host
   token                = var.databricks_token
-  skip_verify          = var.skip_validation  # Skip SSL verification if validation is disabled
-  debug_truncate_bytes = 2048                 # Increase debug output size
-  debug_headers        = true                 # Include headers in debug output
+  skip_verify          = var.skip_validation # Skip SSL verification if validation is disabled
+  debug_truncate_bytes = 2048                # Increase debug output size
+  debug_headers        = true                # Include headers in debug output
 
   # Uncomment for Azure Databricks authentication
   # azure_workspace_resource_id = var.azure_workspace_id
